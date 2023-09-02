b1639f32e090d68d4b40f80244032f4b76e1bd06
Sebastianhidalgo231 <sebastianhidalgook@gmail.com>
2023-09-02 12:22:32 -0300

    se agregaro barra de navegacion y mejoro estilos de los formularios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import LoginForm from './LoginForm';
-import './Form.css';
+import "./App.css";
 import RegUsuario from './Componentes/registro';
 import RecuperarUsuario from './Componentes/recupero';
+import Acerca from './Componentes/Acerca';
+import Preguntas from './Componentes/preguntas';
+import Contacto from './Componentes/contacto';
 
 function App() {
   return (
     <Router>
+       <div className="nav1">
+        <div className="barnav">
+        <nav>
+          <ul>
+            <li>
+              <Link className="nav" to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link className="nav" to="/Acerca">Acerca de</Link>
+            </li>
+            <li>
+              <Link className="nav" to="/preguntas">Preguntas Frecuentes</Link>
+            </li>
+            <li>
+              <Link  className="nav" to="/contacto">Contacto</Link> 
+            </li>
+          </ul>
+        </nav>
+        </div>
+        </div>
       <div className="app">
         <Routes>
           <Route path="/" element={<LoginForm />} />
           <Route path="/registro" element={<RegUsuario />} />
           <Route path="/recupero" element={<RecuperarUsuario />} />
+          <Route path="/Acerca" element={<Acerca />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/preguntas" element={<Preguntas />} />
+
         </Routes>
       </div>
     </Router>
---
+++ src/Componentes/Acerca.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './estilo.css'; // Importa tus estilos CSS
+
+function Acerca() {
+  return (
+    <div className="acercade">
+ <div className="loader">
+      <div className="circle">
+        <div className="dot"></div>
+        <div className="outline"></div>
+      </div>
+      <div className="circle">
+        <div className="dot"></div>
+        <div className="outline"></div>
+      </div>
+      <div className="circle">
+        <div className="dot"></div>
+        <div className="outline"></div>
+      </div>
+      <div className="circle">
+        <div className="dot"></div>
+        <div className="outline"></div>
+      </div>
+    </div>
+      <h1>Proximamente.Estamos codeando</h1>
+
+    </div>
+  );
+}
+
+export default Acerca;
